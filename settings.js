479b181ac48ec80e09a34d74bfde1c194ba4dd7a
Mr Smile Modds <uceyheroku@gmail.com>
2026-04-13 12:09:55 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '❰❴ ⎈ＱＵＥＥＮ ＤＩＶＡ⎈ ❵❱',
+  author: '‎',
+  botName: "❰❴ ⎈ＱＵＥＥＮ ＤＩＶＡ⎈ ❵❱",
+  botOwner: 'ＭＲ ＳＭＩＬＥ', // Your name
+  ownerNumber: '97125567072', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/Neurotechofficial7/Neurotech-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
